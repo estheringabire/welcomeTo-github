2019c4e2cbd0cf17a5f87fadf2ff2e80871d91fa
User <User@UB-HQ-IT-05.ungukabank.local>
2023-04-12 10:57:50 +0200

    login

---
+++ login.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+    <head>
+        <style>
+form{
+    backgraund_color:cyan,
+
+}
+
+        </style>
+        <title></title>
+</head>
+ <body>
+    <form method="POST">
+        <input type="text" name="firstname"><br><br>
+        <input type="text" name="lastname"><br><br>
+        <input type="text" name="username"><br><br> 
+        <input type="password" name="password"><br><br>
+        <button name="submit" value="login">login</button>
+        <button name="reset" value="rest">reset</button>
+       /logon pafes 
+</form>
+</body>
+</html>  
